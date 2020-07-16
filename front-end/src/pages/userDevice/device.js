cbd9507e11b279182b546a6a19b293d446223cb0
LMF <carlos.talero.jacome@gmil.com>
2020-07-15 21:46:06 -0500

    UI picker for device section, now works

--- front-end/src/pages/userDevice/device.js
+++ front-end/src/pages/userDevice/device.js
@@ -6,55 +6,56 @@ import HaloUI from './HaloUI';
 import UserDeviceSkeleton from '../../utilities/UserDeviceSkeleton';
 
 // Redux stuff
+//import store from '../../redux/store';
 import { connect } from 'react-redux';
+import { getUserDevices } from '../../redux/actions/userDevicesActions';
 
 // switch case
 class Device extends Component {
 
-    // if the device is active
-    findActiveDevice= () => {
+    componentWillMount(){
+        this.props.dispatch(getUserDevices());
+    }
+
+    render(){
+
         const {
             userDevices,
             loading
         } = this.props;
 
-        // find the active one
-        const anyActive = userDevices.active.find(
-            (userDevice) => userDevice.active === true
-        )
         // devices ids
         const HALO = 'MZInC971tJYurv3OYzjR';
         const HILDA = 'gE2ySDQaMymbZe0r6KEH';
-
-        // check if ther anyone active
-        if(anyActive){
-            switch(anyActive.deviceId){
-                case HALO:
-                        // specific component
-                        let UIHildaMarkup = !loading ? (
-                            <HildaUI/>
-                            ) : (
-                                <UserDeviceSkeleton/>
-                            );
-                        return(UIHildaMarkup);
-                case HILDA:
-                        // specific component
-                        let UIHaloMarkup = !loading ? (
-                            <HaloUI/>
-                            ) : (
-                                <UserDeviceSkeleton/>
-                            );
-                        return(UIHaloMarkup);
-                default:
-                    return null;
-            }
+        // pick active one
+        const approved = userDevices.filter(userDevice => userDevice.active === true);
+        // pick his deviceId
+        let result = approved.map(({ deviceId }) => deviceId)
+        // show data
+        console.log(result);
+        // pick UI
+        switch(result[0]){
+            case HALO:
+                // specific component
+                let UIHildaMarkup = !loading ? (
+                    <HaloUI/>
+                    ) : (
+                        <UserDeviceSkeleton/>
+                    );
+                return(UIHildaMarkup);
+            case HILDA:
+                // specific component
+                let UIHaloMarkup = !loading ? (
+                    <HildaUI/>
+                    ) : (
+                        <UserDeviceSkeleton/>
+                    );
+                return(UIHaloMarkup);
+            default:
+                return null;
         }
-    };
-
-    render(){
-        {return this.findActiveDevice}
-    }    
-
+        
+    }
 }
 
 const mapStateToProps = (state) => ({
@@ -62,4 +63,5 @@ const mapStateToProps = (state) => ({
     
 })
 
+//export default Device;
 export default connect(mapStateToProps)(Device);
