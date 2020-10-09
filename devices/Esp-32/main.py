27c404b4d5213f265f9eebe0717dd302ff4272c0
LMF <carlos.talero.jacome@gmil.com>
2020-10-09 18:24:02 -0500

    some util comments about the main.py file to the mqtt thing

--- devices/Esp-32/main.py
+++ devices/Esp-32/main.py
@@ -18,9 +18,11 @@ sta_if = network.WLAN(network.STA_IF)
 led_pin = machine.Pin(config.device_config['led_pin'], Pin.OUT) #built-in LED pin
 led_pin.value(1)
 
+# function to handle the receive  message, always show something about config topic
 def on_message(topic, message):
     print((topic,message))
 
+# function to establish and eval the status of the connection 
 def connect():
     if not sta_if.isconnected():
         print('connecting to network...')
@@ -30,6 +32,7 @@ def connect():
             pass
     print('network config: {}'.format(sta_if.ifconfig()))
 
+# take care about the time for the messaging ----> i guess
 def set_time():
     ntptime.settime()
     tm = utime.localtime()
@@ -37,24 +40,26 @@ def set_time():
     machine.RTC().datetime(tm)
     print('current time: {}'.format(utime.localtime()))
 
+# 
 def b42_urlsafe_encode(payload):
     return string.translate(b2a_base64(payload)[:-1].decode('utf-8'),{ ord('+'):'-', ord('/'):'_' })
 
+# creation of the jwt
 def create_jwt(project_id, private_key, algorithm, token_ttl):
     print("Creating JWT...")
+    # the actual token
     private_key = rsa.PrivateKey(*private_key)
-
     # Epoch_offset is needed because micropython epoch is 2000-1-1 and unix is 1970-1-1. Adding 946684800 (30 years)
     epoch_offset = 946684800
+    # duration
     claims = {
-            # The time that the token was issued at
-            'iat': utime.time() + epoch_offset,
-            # The time the token expires.
-            'exp': utime.time() + epoch_offset + token_ttl,
-            # The audience field should always be set to the GCP project id.
-            'aud': project_id
+        # The time that the token was issued at
+        'iat': utime.time() + epoch_offset,
+        # The time the token expires.
+        'exp': utime.time() + epoch_offset + token_ttl,
+        # The audience field should always be set to the GCP project id.
+        'aud': project_id
     }
-
     #This only supports RS256 at this time.
     header = { "alg": algorithm, "typ": "JWT" }
     content = b42_urlsafe_encode(ujson.dumps(header).encode('utf-8'))
@@ -62,6 +67,7 @@ def create_jwt(project_id, private_key, algorithm, token_ttl):
     signature = b42_urlsafe_encode(rsa.sign(content,private_key,'SHA-256'))
     return content+ '.' + signature #signed JWT
 
+# function to create the mqtt client
 def get_mqtt_client(project_id, cloud_region, registry_id, device_id, jwt):
     """Create our MQTT client. The client_id is a unique string that identifies
     this device. For Google Cloud IoT Core, it must be in the format below."""
@@ -74,20 +80,22 @@ def get_mqtt_client(project_id, cloud_region, registry_id, device_id, jwt):
     client.subscribe('/devices/{}/commands/#'.format(device_id), 1)
     return client
 
+# run the eval and connect function
 connect()
-#Need to be connected to the internet before setting the local RTC.
+# Need to be connected to the internet before setting the local RTC.
 set_time()
 
 jwt = create_jwt(config.google_cloud_config['project_id'], config.jwt_config['private_key'], config.jwt_config['algorithm'], config.jwt_config['token_ttl'])
 client = get_mqtt_client(config.google_cloud_config['project_id'], config.google_cloud_config['cloud_region'], config.google_cloud_config['registry_id'], config.google_cloud_config['device_id'], jwt)
 
 while True:
+    # telemetry dict
     message = {
         "device_id": config.google_cloud_config['device_id'],
         "temp": esp32.raw_temperature()
     }
     print("Publishing message "+str(ujson.dumps(message)))
-    led_pin.value(1)
+    led_pin.value(2)
     mqtt_topic = '/devices/{}/{}'.format(config.google_cloud_config['device_id'], 'events')
     client.publish(mqtt_topic.encode('utf-8'), ujson.dumps(message).encode('utf-8'))
     led_pin.value(0)
